8bc4c58fe7efae4d036686a5a5073f193c1d5618
Geoffrey <126630931+Gstruss@users.noreply.github.com>
2024-10-01 20:58:51 -0500

    Add files via upload

--- MLPrediction.py
+++ MLPrediction.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_log_error
+from sklearn.metrics import mean_squared_log_error, r2_score
 
 from grafi_moshe import CapturaDatos
 class PrepareData:
@@ -16,7 +16,33 @@ class PrepareData:
 
     def pandasData(self):
         df = pd.DataFrame(self.listData,columns=['Year','Quarter','Provider','Income','amountSMS'])
-        print(df)
+        df['Year'] = pd.to_datetime(df['Year'])
+        df['Quarter'] = df['Quarter'].astype('category').cat.codes
+        df['Provider'] = df['Provider'].astype('category').cat.codes
+
+        x = df[['Year','Quarter','Provider','amountSMS']]
+        y = df['Income']
+
+        x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,random_state=42)
+
+        model = LinearRegression()
+        model.fit(x_train,y_train)
+
+        y_pred = model.predict(x_test)
+
+        mse = mean_squared_log_error(y_test,y_pred)
+
+        r2 = r2_score(y_test,y_pred)
+
+        print(f"Mean error: {mse}")
+        print(f"R-squared error: {r2}")
+
+        plt.scatter(y_test,y_pred)
+        plt.xlabel("% real income")
+        plt.ylabel("Income prediction")
+        plt.title("Regresion: Incomes")
+        plt.show()
+
 
 prueba = PrepareData()
 prueba.PrepareJson()
